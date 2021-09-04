63505e9394edb4377fe408aa9b5df11b44c8d2b7
Udit Takkar <udit.07814802719@cse.mait.ac.in>
2021-09-04 17:41:38 +0530

    ✅ Set up Client -> Next js -> tailwind configured -> prettier configured ->eslint configured ->typescript configured

---
+++ client/pages/index.tsx
@@ -0,0 +1,11 @@
+import Head from "next/head";
+import utilStyles from "../styles/utils.module.css";
+import Link from "next/link";
+
+export default function Home({ allPostsData }) {
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  );
+}
